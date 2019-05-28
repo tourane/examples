a589a221953d0e0bcf142254b331137a3c34089e
pnhung177 <pnhung177@acegik.net>
2019-05-28 11:32:39 +0700

    The first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tourane examples
+
+## Installation
+
+Checkout the source code of example from github:
+
+```shell
+git clone https://github.com/tourane/examples.git
+```
+
+```shell
+cd examples
+```
+
+Install dependencies:
+
+```shell
+composer install
+```
+
+## Examples
+
+### Logging
+
+```shell
+php logging.php
+```
+
---
+++ logging.php
@@ -0,0 +1,55 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Tourane\ProxyKit\Adapter;
+
+class MongoClient {
+  public function find($query, $opts) {
+    $items = array( 1, 3, 5, 7, 11);
+    return array(
+      "items" => $items,
+      "total" => count($items)
+    );
+  }
+  public function close($opts) {
+    return true;
+  }
+}
+
+$db = new MongoClient();
+
+// Define the tracer object
+$tracer = new Adapter(array(
+  "logging" => array(
+    "file" => array(
+      "dir" => dirname(__FILE__ ) . '/log',
+      "filename" => "example.log"
+    ),
+    "extra" => array(
+      "ProcessId" => true
+    )
+  )
+));
+
+// Wrap the original object
+$db = $tracer->wrap($db, array(
+  "loggingMethods" => array(
+    "find" => array(),
+    "close" => array(
+      "logArguments" => true,
+      "logReturnValue" => true
+    )
+  )
+));
+
+// Keep the source code unchanged
+
+$result = $db->find(array("type" => "prime", "max" => 15), null);
+printf("find: %s\n", json_encode($result));
+
+try {
+  $db->close(array());
+} catch (Exception $e) {
+  $tracer->getLogger()->error("Error: " . $e->getMessage());
+}
+?>
